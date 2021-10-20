254fea80647d5b914298c6ba0b403e3ca426fd76
GancarczykKarolina <karolinagancarczyk96@gmail.com>
2021-10-20 22:22:29 +0200

    happy path finished

--- cypress/integration/car-rent.feature
+++ cypress/integration/car-rent.feature
@@ -8,3 +8,14 @@ Feature: Car Rent
         Given the user visits the Main Page
         When the user selects 'Poland' from 'Country' dropdown
         And the user selects 'Cracow' from 'City' dropdown
+        And the user types 'A-Class' into 'Model' text field
+        And the user types '2020-10-10' into 'Pickup' text field
+        And the user types '2020-11-11' into 'DropOff' text field
+        And the user clicks on 'Search' button
+        And the user clicks on Rent button for 'Davis-Garcia'
+        And the user clicks on 'Rent!' element with 'btn btn-primary' 'all' class name
+        And the user types 'Dawid' into 'Name' text field
+        And the user types 'Kudzia' into 'Last_Name' text field
+        And the user types '123' into 'Card_Number' text field
+        And the user types 'kudzia.dawid@gmail.com' into 'Email' text field
+        And the user clicks on 'Rent' button
--- cypress/integration/car-rent/carRent.js
+++ cypress/integration/car-rent/carRent.js
@@ -7,3 +7,27 @@ Given('the user visits the Main Page', () => {
 When('the user selects {string} from {string} dropdown', (dropdownValue, dropdownName) => {
     cy.get('#'+dropdownName.toLowerCase()).select(dropdownValue)
 })
+
+When('the user types {string} into {string} text field', (textFieldValue, textFieldName) => {
+    cy.get('#'+textFieldName.toLowerCase()).type(textFieldValue)
+})
+
+When('the user clicks on {string} button', (buttonText) => {
+    cy.get('button').contains(buttonText).click()
+})
+
+When('the user clicks on {string} element with {string} {string} class name', (elementText, className, oneOrAllClasses) => {
+    switch(oneOrAllClasses) {
+        case 'one': 
+            cy.get('.'+ className).contains(elementText).click()
+            break
+        case 'all': 
+            cy.get('[class="'+ className +'"]').contains(elementText).click()
+            break
+        default: throw new Error('Second string accepts only "one" or "all" value')
+    }
+})
+
+When('the user clicks on Rent button for {string}', (itemValue) => {
+    cy.get('#search-results').find('tr').contains(new RegExp('^' + itemValue + '$', 'g')).parent().find('td').last().click()
+})
